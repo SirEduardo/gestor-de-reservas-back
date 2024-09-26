b3715d9da597c0deb8b79138478b203a98b156a6
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-26 12:28:54 +0200

    Create users.js

---
+++ back/src/api/models/users.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    userName: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      required: false,
+      enum: ["admin", "client"],
+      default: "client",
+    },
+    creation_date: { type: Date, default: Date.now },
+  },
+  {
+    timestamps: true,
+    collection: "users",
+  }
+);
+
+userSchema.pre("save", function (next) {
+  this.password = bcrypt.hashSync(this.password, 10);
+  next();
+});
+
+const User = mongoose.model("users", userSchema, "users");
+module.exports = User;
