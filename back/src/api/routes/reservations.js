89d82e3323f084bd85aad2679e4c8366ecaf821d
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-26 12:28:56 +0200

    Create reservations.js

---
+++ back/src/api/routes/reservations.js
@@ -0,0 +1,20 @@
+const { isAuth } = require("../../middlewares/auth");
+const {
+  getReservationByUser,
+  getReservationsByRestaurant,
+  createReservation,
+  cancelReservation,
+} = require("../controllers/reservations");
+
+const reservationRoutes = require("express").Router();
+
+reservationRoutes.get("/my-reservation", [isAuth], getReservationByUser);
+reservationRoutes.get(
+  "/restaurant/:restaurantId",
+  [isAuth],
+  getReservationsByRestaurant
+);
+reservationRoutes.post("/", [isAuth], createReservation);
+reservationRoutes.put("/:id/cancel", [isAuth], cancelReservation);
+
+module.exports = reservationRoutes;
