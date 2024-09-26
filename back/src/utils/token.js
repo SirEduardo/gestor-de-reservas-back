c22177378ae9fb3cf4159bbeef1db4dadf3fcf0f
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-26 12:29:07 +0200

    Create token.js

---
+++ back/src/utils/token.js
@@ -0,0 +1,10 @@
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1y" });
+};
+const verifyToken = (token) => {
+  return jwt.verify(token, process.env.JWT_SECRET);
+};
+
+module.exports = { generateToken, verifyToken };
