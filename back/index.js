ec7920d4631e814eea37249915b7077be7d186ed
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-26 18:39:10 +0200

    Update index.js

--- back/index.js
+++ back/index.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const { connectDB } = require("./src/config/db");
+const cors = require("cors");
 const userRoutes = require("./src/api/routes/users");
 const restaurantRoutes = require("./src/api/routes/restaurant");
 const reservationRoutes = require("./src/api/routes/reservations");
 
 const app = express();
+app.use(cors());
+app.options("*", cors());
+app.use(express.json());
 
 connectDB();
 
