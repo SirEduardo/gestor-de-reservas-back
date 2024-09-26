833741734e8f972596f2ec5f9b80d573e5c05d1d
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-26 12:28:46 +0200

    Create comment.js

---
+++ back/src/api/models/comment.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const commmentSchema = new Schema(
+  {
+    user: { trype: Schema.Types.ObjectId, ref: "users", required: true },
+    restaurant: {
+      type: Schema.Types.ObjectId,
+      ref: "restaurants",
+      required: true,
+    },
+    comment: { type: String, required: true, trim: true },
+    rating: { type: Number, rqeuired: true, min: 0, max: 5 },
+    craetion_date: { type: Date, default: Date.now },
+  },
+  {
+    collection: "comment",
+  }
+);
+
+const Comment = mongoose.model("comments", commmentSchema, "comments");
+module.exports = Comment;
