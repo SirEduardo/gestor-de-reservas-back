9bd7de82890225fc3241a897579c0e2ae3182db5
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-26 12:29:05 +0200

    Create auth.js

---
+++ back/src/middlewares/auth.js
@@ -0,0 +1,28 @@
+const User = require("../api/models/users");
+const { verifyToken } = require("../utils/token");
+
+const isAuth = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    if (!token) {
+      return res.status(401).json({ message: "Token not provided" });
+    }
+    const parsedtoken = token.replace("Bearer ", "");
+    const decodedtoken = verifyToken(parsedtoken);
+
+    if (!decodedtoken.id) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+
+    const user = await User.findById(decodedtoken.id);
+    if (user.role === "admin") {
+      user.password = undefined;
+      req.user = user;
+      next();
+    }
+  } catch (error) {
+    return res.status(401).json("Unauthorized");
+  }
+};
+
+module.exports = { isAuth };
