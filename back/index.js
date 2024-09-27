ce417a46ec0dbe3eedf97ea9b9134843f8cca491
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-27 11:47:08 +0200

    Update index.js

--- back/index.js
+++ back/index.js
@@ -2,11 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const { connectDB } = require("./src/config/db");
 const cors = require("cors");
+const cloudinary = require("cloudinary").v2;
 const userRoutes = require("./src/api/routes/users");
 const restaurantRoutes = require("./src/api/routes/restaurant");
 const reservationRoutes = require("./src/api/routes/reservations");
 
 const app = express();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
 app.use(cors());
 app.options("*", cors());
 app.use(express.json());
