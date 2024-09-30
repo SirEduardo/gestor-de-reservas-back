1098c9389102b880270ef4b26c6df340b31328a2
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-30 15:33:40 +0200

    Update comments.js

--- back/src/api/controllers/comments.js
+++ back/src/api/controllers/comments.js
@@ -1,4 +1,5 @@
 const Comment = require("../models/comment");
+const Restaurant = require("../models/restaurant");
 
 const getComments = async (req, res, next) => {
   try {
@@ -11,18 +12,40 @@ const getComments = async (req, res, next) => {
 
 const createComments = async (req, res, next) => {
   try {
-    const { restaurant, text } = req.body;
-    const user = req.user.id;
+    const { restaurant } = req.params;
+    const { text, rating } = req.body;
+    const user = req.user._id;
+
+    if (rating < 1 || rating > 5) {
+      return res
+        .status(400)
+        .json({ message: "Rating must be between 1 and 5" });
+    }
+    if (!text || !rating || typeof rating !== "number") {
+      return res.status(400).json({ message: "Invalid input data" });
+    }
     const newComment = new Comment({
       user,
       restaurant,
       text,
+      rating,
     });
     const commentSaved = await newComment.save();
-    return res
-      .status(201)
-      .json({ message: "Comment created", commentSaved: commentSaved });
-  } catch (error) {}
+    const restaurantDoc = await Restaurant.findById(restaurant);
+    if (!restaurantDoc) {
+      return res.status(404).json({ message: "Restaurant not found" });
+    }
+    restaurantDoc.comments.push(commentSaved._id);
+    restaurantDoc.rating_number += 1;
+    restaurantDoc.average_rating =
+      (restaurantDoc.average_rating * (restaurantDoc.rating_number - 1) +
+        rating) /
+      restaurantDoc.rating_number;
+    await restaurantDoc.save();
+    return res.status(201).json({ message: "Comment created", commentSaved });
+  } catch (error) {
+    return res.status(500).json({ message: "Failed to create comment", error });
+  }
 };
 
 const deleteComments = async (req, res, next) => {
