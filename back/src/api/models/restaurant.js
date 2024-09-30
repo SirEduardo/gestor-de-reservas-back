a2f12681d63c0b5951b2b9f2fce71f37de41fbad
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-30 15:33:55 +0200

    Update restaurant.js

--- back/src/api/models/restaurant.js
+++ back/src/api/models/restaurant.js
@@ -5,7 +5,7 @@ const restaurantSchema = new Schema(
   {
     name: { type: String, required: true, trim: true },
     location: { type: String, required: true, trim: true },
-    owner: { type: Schema.Types.ObjectId, ref: "users", required: true },
+    owner: { type: Schema.Types.ObjectId, ref: "user", required: true },
     category: {
       type: String,
       enum: [
@@ -21,15 +21,16 @@ const restaurantSchema = new Schema(
       required: true,
     },
     img: { type: String },
-    menu: { type: Schema.Types.ObjectId, ref: "menus" },
+    menu: { type: Schema.Types.ObjectId, ref: "menu" },
     telephone: { type: String, required: true, trim: true },
     schedule: {
       opening: { type: String, required: true },
       closing: { type: String, required: true },
     },
     creation_date: { type: Date, default: Date.now },
-    average_rating: { type: Number, default: 0, min: 0, max: 5 },
-    raing_number: { type: Number, default: 0 },
+    average_rating: { type: Number, min: 0, max: 5, default: 0 },
+    rating_number: { type: Number, default: 0 },
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "comment" }],
   },
   {
     collection: "restaurants",
@@ -37,7 +38,7 @@ const restaurantSchema = new Schema(
 );
 
 const Restaurant = mongoose.model(
-  "restaurants",
+  "restaurant",
   restaurantSchema,
   "restaurants"
 );
