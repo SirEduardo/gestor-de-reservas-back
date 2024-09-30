22ecb75363eafaf492d3cc36b6ab83ea8f642706
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-30 15:34:11 +0200

    Update auth.js

--- back/src/middlewares/auth.js
+++ back/src/middlewares/auth.js
@@ -3,7 +3,7 @@ const { verifyToken } = require("../utils/token");
 
 const isAuth = async (req, res, next) => {
   try {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization.split(" ")[1];
     if (!token) {
       return res.status(401).json({ message: "Token not provided" });
     }
