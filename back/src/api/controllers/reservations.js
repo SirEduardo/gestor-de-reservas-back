da9135a48a04962338734fc8ddb2360882837427
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-03 15:15:35 +0200

    Update reservations.js

--- back/src/api/controllers/reservations.js
+++ back/src/api/controllers/reservations.js
@@ -1,16 +1,23 @@
 const Reservation = require("../models/reservations");
 const Restaurant = require("../models/restaurant");
+const User = require("../models/users");
 
 const createReservation = async (req, res, next) => {
   try {
     const { restaurant, booking_date, time, n_persons } = req.body;
     const user = req.user.id;
-    if ((!restaurant, !booking_date, !time, !n_persons)) {
+    console.log(user);
+
+    if ((restaurant, !booking_date, !time, !n_persons)) {
       return res.status(400).json("All fields are required");
     }
     const restaurantDoc = await Restaurant.findById(restaurant);
     if (!restaurantDoc) {
       return res.status(400).json("This restaurant does not exist");
+    }
+    const userInfo = await User.findById(user);
+    if (!userInfo) {
+      return res.status(404).json({ message: "User not found" });
     }
     const newReservation = new Reservation({
       user,
@@ -23,7 +30,10 @@ const createReservation = async (req, res, next) => {
 
     const reservationSaved = await newReservation.save();
 
+    userInfo.reservations.push(reservationSaved._id);
     restaurantDoc.reservations.push(reservationSaved._id);
+
+    await userInfo.save();
     await restaurantDoc.save();
     return res.status(200).json({
       message: "Reserve successfully created",
@@ -38,7 +48,7 @@ const getReservationByUser = async (req, res, next) => {
   try {
     const user = req.user.id;
     const reserve = await Reservation.find({ user }).populate(
-      "restaurants",
+      "restaurant",
       "name location"
     );
     return res.status(200).json({ reserve });
