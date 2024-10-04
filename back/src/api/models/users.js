cad5b265b86f56dda633935c474332dfede2210f
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-04 12:26:36 +0200

    Update users.js

--- back/src/api/models/users.js
+++ back/src/api/models/users.js
@@ -25,8 +25,14 @@ const userSchema = new Schema(
   }
 );
 
-userSchema.pre("save", function (next) {
-  this.password = bcrypt.hashSync(this.password, 10);
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    try {
+      this.password = await bcrypt.hash(this.password, 10);
+    } catch (error) {
+      return next(error);
+    }
+  }
   next();
 });
 
