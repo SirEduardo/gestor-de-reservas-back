7da8fe6f92db3f4ba6e374ed392b0d4b077fc020
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-04 22:05:33 +0200

    Update users.js

--- back/src/api/models/users.js
+++ back/src/api/models/users.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema(
   {
     userName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, trim: true },
     password: { type: String, required: true },
     role: {
