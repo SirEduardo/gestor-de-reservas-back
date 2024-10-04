7752cdac77efcdf22c15a49033816f5efbd8ad24
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-04 12:26:53 +0200

    Update token.js

--- back/src/utils/token.js
+++ back/src/utils/token.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1y" });
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "1d" });
 };
 const verifyToken = (token) => {
   return jwt.verify(token, process.env.JWT_SECRET);
